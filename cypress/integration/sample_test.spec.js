8510f1825878d89db7f9d439e6441d0ad7c196f0
Gabriel Leme <gabriel@easycomtec.com>
2021-10-10 12:30:24 -0300

    new tests created

---
+++ cypress/integration/sample_test.spec.js
@@ -0,0 +1,24 @@
+///<reference types = "cypress"/>
+
+describe('Common usage tests to integrate with Jenkins', () => {
+    const main_page = "http://automationpractice.com/index.php"
+
+    beforeEach(() => {
+        cy.visit(main_page)
+    });
+    
+    it('Should validate the title', () => {
+        cy.title().should('contain', 'My Store')
+    });
+
+    it('Should send a support message', () => {
+        cy.get('#contact-link > a').click()
+        cy.get('#id_contact').select('Webmaster')
+        cy.get('#email').type('mail.test@test.ts')
+        cy.get('#id_order').type('12355')
+        cy.get('#message').type('A commom message only to perform tests')
+        cy.get('#submitMessage > span').click()
+        cy.get('.alert').should('have.text', 'Your message has been successfully sent to our team.')
+    });
+
+})
